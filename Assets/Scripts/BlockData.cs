20afa606524e3ac4c534dc94b68088d48ef76aee
TokensPony <cmtherealmccoy@gmail.com>
2018-01-23 13:42:39 -0600

    First Commit

---
+++ Assets/Scripts/BlockData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockData : MonoBehaviour {
+
+	public string color;
+	public Vector3 position;
+	public Material texture;
+
+	public float boardWidth;
+	private float xOffset;
+	private float offset = .15f;
+
+	public void Start(){
+		boardWidth = GameObject.Find ("BoutManager").GetComponent<BoutManager> ().boardWidth;
+		xOffset = GameObject.Find ("BoutManager").GetComponent<BoutManager> ().xOffset;
+	}
+
+	void OnMouseDrag()
+	{
+		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+		Vector3 pos_move = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
+		float snapPosition = ((pos_move.x - Mathf.Floor (pos_move.x)) > .5f) ? Mathf.Ceil (pos_move.x) : Mathf.Floor (pos_move.x);
+		snapPosition = (Mathf.Abs (snapPosition) > boardWidth / 2) ? transform.position.x : snapPosition;
+		/*if (snapPosition > 0f) {
+			snapPosition += (xOffset * (boardWidth/2));
+		} else if (snapPosition < 0f) {
+			snapPosition -= (xOffset * (boardWidth/2));
+		}*/
+		if (snapPosition != 0f && Mathf.Abs(snapPosition) + (xOffset* Mathf.Abs(snapPosition)) < (boardWidth/2)+ xOffset*boardWidth) {
+			snapPosition += (xOffset * snapPosition);
+		}
+		transform.position = new Vector3 (snapPosition, pos_move.y, -2f);
+	}
+
+	void OnMouseUp(){
+		transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
+		this.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+	}
+}
---
+++ Assets/Scripts/BoutManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoutManager : MonoBehaviour {
+
+	public GameObject block;
+	public GameObject[][] blocks;
+
+	public float boardWidth;
+	public float xOffset = .15f;
+	private float spawnHeight = 1f;
+
+	// Use this for initialization
+	void Start () {
+		for (int x = 0; x < 3; x++) {
+			createBlock ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void createBlock(){
+		Instantiate (block);
+		Vector3 spawnPoint = new Vector3 (Mathf.Floor (Random.value * boardWidth - (boardWidth/2f)), Mathf.Floor (Random.value * 10f) + spawnHeight++, 0f);
+		if (spawnPoint.x > 0f) {
+			spawnPoint.x += (xOffset*(boardWidth/2));
+		} else if (spawnPoint.x < 0f) {
+			spawnPoint.x -= (xOffset*(boardWidth/2));
+		}
+		block.transform.position = spawnPoint;
+	}
+
+
+}
