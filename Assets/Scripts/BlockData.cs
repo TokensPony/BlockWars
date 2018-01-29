42011361628e522c6de066f5a0857a3ac37318f8
TokensPony <cmtherealmccoy@gmail.com>
2018-01-28 18:01:18 -0600

    Started adding Matching Algorithm. Currently triggered by checkbox. Not currently consistent, needs work.

--- Assets/Scripts/BlockData.cs
+++ Assets/Scripts/BlockData.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class BlockData : MonoBehaviour {
+
+public class BlockData : MonoBehaviour{
 
 	public string color;
 	public Vector3 position;
@@ -40,6 +42,17 @@ public class BlockData : MonoBehaviour {
 		}
 		transform.position = new Vector3 (snapPosition, pos_move.y, -2f);
 	}
+
+	/*public void OnDrag(PointerEventData ped){
+		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+		Vector3 pos_move = Camera.main.ScreenToWorldPoint (new Vector3 (ped.position.x, ped.position.y, distance_to_screen));
+		float snapPosition = ((pos_move.x - Mathf.Floor (pos_move.x)) > .5f) ? Mathf.Ceil (pos_move.x) : Mathf.Floor (pos_move.x);
+		snapPosition = (Mathf.Abs (snapPosition) > boardWidth / 2) ? transform.position.x : snapPosition;
+		if (snapPosition != 0f && Mathf.Abs(snapPosition) + (xOffset* Mathf.Abs(snapPosition)) < (boardWidth/2)+ xOffset*boardWidth) {
+			snapPosition += (xOffset * snapPosition);
+		}
+		transform.position = new Vector3 (snapPosition, pos_move.y, -2f);
+	}*/
 
 	void OnMouseUp(){
 		transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
--- Assets/Scripts/BoutManager.cs
+++ Assets/Scripts/BoutManager.cs
@@ -41,8 +41,6 @@ public class BoutManager : MonoBehaviour {
 		}
 		newBlock.transform.position = spawnPoint;
 		newBlock.GetComponent<BlockData> ().gridCoord = new Vector2 (xPos, yPos);
-		//Debug.Log (newBlock.GetComponent<BlockData> ().spawnY);
-		//Debug.Log ("X: " + xPos + "Y: " + yPos);
 		blocks [yPos, xPos] = newBlock;
 	}
 
@@ -79,6 +77,13 @@ public class BoutManager : MonoBehaviour {
 		for (int y = blocks.GetLength (0)-1; y >= 0; y--) {
 			for (int x = 0; x < blocks.GetLength (1); x++) {
 				if (blocks [y, x] != null) {
+					/*if (blocks [y, x].GetComponent<Renderer>().material == textures[0]) {
+						grid += 'R';
+					}else if (blocks [y, x].GetComponent<Renderer>().material == textures [1]) {
+						grid += 'B';
+					}else if (blocks [y, x].GetComponent<Renderer>().material == textures [2]) {
+						grid += 'G';
+					}*/
 					grid += 'X';
 				} else {
 					grid += 'O';
@@ -89,18 +94,85 @@ public class BoutManager : MonoBehaviour {
 		Debug.Log (grid);
 	}
 
+	/*Checks if any blocks in the grid have been "marked". This is being used as a temporary
+	means of triggering the match search.*/
 	public void isMarked(){
 		for (int y = blocks.GetLength (0)-1; y >= 0; y--) {
 			for (int x = 0; x < blocks.GetLength (1); x++) {
+				if (blocks [y, x] != null && blocks[y,x].GetComponent<BlockData>().marked &&
+					matchMade (blocks [y, x])) {
+					//if (matchMade (blocks [y, x])) {
+						removeMarked ();
+						//collapseBoard ();
+						//printGrid ();
+				}
+			}
+		}
+	}
+
+	public void removeMarked(){
+		for (int y = blocks.GetLength (0)-1; y >= 0; y--) {
+			for (int x = 0; x < blocks.GetLength (1); x++) {
 				if (blocks [y, x] != null && blocks[y,x].GetComponent<BlockData>().marked) {
-					//Debug.Log ("Before: " + blocks [y, x]);
 					Destroy (blocks [y, x].gameObject);
 					blocks [y, x] = null;
-					//Debug.Log ("After: " + blocks [y, x]);
-					printGrid ();
+					//matchMade (blocks [y, x]);
+					//collapseBoard ();
+					//printGrid ();
 				} else {
 				}
 			}
 		}
+		collapseBoard ();
+		printGrid ();
+	}
+
+	/*Readjusts the positions of the objects in the Grid after a move has been made.*/
+	public void collapseBoard(){
+		GameObject[,] temp = new GameObject[12,9];
+		int yIndex = 0;
+		for (int x = 0; x < blocks.GetLength (1); x++) {
+			for (int y = 0; y < blocks.GetLength(0); y++) {
+				if (blocks [y, x] != null) {
+					temp [yIndex, x] = blocks [y, x];
+					temp [yIndex, x].GetComponent<BlockData> ().gridCoord = new Vector2 (yIndex, x);
+					yIndex++;
+				}
+			}
+			yIndex = 0;
+		}
+		blocks = temp;
+	}
+
+	public bool matchMade(GameObject startBlock){
+		Material startMat = startBlock.GetComponent<Renderer>().sharedMaterial;
+		//Debug.Log (startMat);
+		startBlock.GetComponent<BlockData> ().marked = true;
+		Vector2 pos = startBlock.GetComponent<BlockData> ().gridCoord;
+		bool found = false;
+		Debug.Log (blocks [(int)pos.y -1 , (int)pos.x]);
+		/*Down*/
+		if (pos.y != 0f &&
+			blocks [(int)pos.y - 1, (int)pos.x] != null &&
+			startMat == blocks [(int)pos.y - 1, (int)pos.x].GetComponent<Renderer>().sharedMaterial &&
+		    !blocks [(int)pos.y - 1, (int)pos.x].GetComponent<BlockData> ().marked) {
+			Debug.Log ("Matched");
+			matchMade (blocks [(int)pos.y - 1, (int)pos.x]);
+			found = true;
+		}
+
+		/*Board search started, having trouble with consistency. Sometimes it sees adjacent blocks,
+		other times not. Need to figure out what it's not seeing. Consider making alternate version
+		of printGrid with a parameter for "YOU ARE HERE"*/
+
+		/*Left*/
+		/*if(pos.x != 0f &&
+			startMat == blocks [(int)pos.y, (int)pos.x-1].GetComponent<Renderer>().sharedMaterial &&
+			!blocks [(int)pos.y, (int)pos.x-1].GetComponent<BlockData> ().marked){
+			matchMade (blocks [(int)pos.y, (int)pos.x-1]);
+			found = true;
+		}*/
+
+		return found;
 	}
 }
